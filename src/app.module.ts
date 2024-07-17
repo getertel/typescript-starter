f01eb8d4a8be4fee0915e5140ed27c1d4be5a03f
getertel <geter3@hotmail.com>
2024-07-17 00:26:32 -0300

    detalhes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModuleModule } from './module/module.module';
+import { ModuleModule } from './coletanea/module/module.module';
 
 @Module({
-  imports: [],
+  imports: [ModuleModule],
   controllers: [AppController],
   providers: [AppService],
 })
